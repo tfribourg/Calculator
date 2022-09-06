bd105b1f13370dc83c7546e6b96913b62c87fdd6
Tyler Fribourg <tyler0153@gmail.com>
2022-09-06 16:28:11 -0400

    Create basic math functions

--- java.js
+++ java.js
@@ -1,1 +1,12 @@
-
+function add(a, b) {
+    return a + b
+}
+const subtract = (a, b) => {
+    return a- b
+}
+function multiply(a, b) {
+    return a * b
+}
+function divide (a, b) {
+    return a % b
+}
