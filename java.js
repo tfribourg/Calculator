c5a74baacfd6efb4f193f02ab0e69f95b360a631
tfribourg <103950978+tfribourg@users.noreply.github.com>
2022-09-15 10:53:16 -0400

    Add display update function

--- java.js
+++ java.js
@@ -1,3 +1,74 @@
+let displayVal = '0'
+
+
+const display = document.querySelector('.display')
+const buttons = document.querySelectorAll('.btn')
+
+function updateDisplay() {
+    display.textContent = displayVal
+}
+
+buttons.forEach(
+
+    btn.addEventListener('click', e)
+)
+
+
+
+
+
+
+
+
+function clear(){
+    document.querySelector('display')
+    .display = ""
+}
+
+function add(a, b) {
+    return a + b
+}
+const subtract = (a, b) => {
+    return a- b
+}
+function multiply(a, b) {
+    return a * b
+}
+function divide (a, b) {
+    return a / b
+}
+
+function operate (a, b, op) {
+    if (op === '+') {
+        return a + b }
+    if (op === '-'){
+        return a - b }
+    if (op === '*'){
+        return a * b }
+    if (op === '/'){
+        return a / b    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function add(a, b) {
     return a + b
 }
